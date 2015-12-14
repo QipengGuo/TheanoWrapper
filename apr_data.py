4a9f61d8b2db91871885285d7cae0e425381066e
Qipeng Guo <qpguo12@fudan.edu.cn>
2015-12-14 21:02:43 +0800

    fix sth in apr

--- apr_data.py
+++ apr_data.py
@@ -9,17 +9,18 @@ class Amazon_data(object):
                         f = NP.load('token.npz')
                         self.emb_data = f['data']
                         self.emb_label = f['label']
+                        self.vocab_map = f['vocab_map']
                 else:
 		        f = open(en_list_name, 'r')
 		        self.en_list = f.readlines()
 		        f.close()
 		        self.read_json(apr_name)
-                        NP.savez('token.npz', data=self.emb_data, label=self.emb_label)
+                        NP.savez('token.npz', data=self.emb_data, label=self.emb_label, vocab_map = self.vocab_map)
                 self.idx = 0
                 self.max_vocab = 20000
                 self.t_idx = 0
-                self.vocab_size = min(self.max_vocab, NP.max(self.emb_data))
-                self.test_size = 5000
+                self.vocab_size = min(self.max_vocab, NP.max(self.emb_data)+1)
+                self.test_size = 2000
                 self.test_emb_data = self.emb_data[-self.test_size:]
                 self.test_emb_label = self.emb_label[-self.test_size:]
                 self.emb_data = self.emb_data[:-self.test_size]
@@ -38,24 +39,35 @@ class Amazon_data(object):
                         self.idx = ed % l
 		if ed > l:
                         if test:         
-			        return NP.concatenate((self.test_emb_data[st:], self.test_emb_data[:ed-l]), axis=0), NP.concatenate((self.test_emb_label[st:], self.test_emb_label[:ed-l]), axis=0)
+                                data = NP.concatenate((self.test_emb_data[st:], self.test_emb_data[:ed-l]), axis=0)
+                                label = NP.concatenate((self.test_emb_label[st:], self.test_emb_label[:ed-l]), axis=0)
+                                return data, label, NP.argmin(data, axis=1)
                         else:
-			        return NP.concatenate((self.emb_data[st:], self.emb_data[:ed-l]), axis=0), NP.concatenate((self.emb_label[st:], self.emb_label[:ed-l]), axis=0)
+			        data = NP.concatenate((self.emb_data[st:], self.emb_data[:ed-l]), axis=0)
+                                label = NP.concatenate((self.emb_label[st:], self.emb_label[:ed-l]), axis=0)
+                                return data, label, NP.argmin(data, axis=1)
+                                
 		else:
                         if test:
-                                return NP.asarray(self.test_emb_data[st:ed]), NP.asarray(self.test_emb_label[st:ed])
+                                data = NP.asarray(self.test_emb_data[st:ed])
+                                label = NP.asarray(self.test_emb_label[st:ed])
+                                return data, label, NP.argmin(data, axis=1)
                         else:
-			        return NP.asarray(self.emb_data[st:ed]), NP.asarray(self.emb_label[st:ed])
+			        data = NP.asarray(self.emb_data[st:ed])
+                                label = NP.asarray(self.emb_label[st:ed])
+                                return data, label, NP.argmin(data, axis=1)
 
         def get_batch(self, batch_size, test=False):
-                data, label = self._get_batch(batch_size, test)
+                data, label, n_len = self._get_batch(batch_size, test)
                 one_hot_data = NP.zeros((batch_size, len(data[0]), self.vocab_size))
                 one_hot_label = NP.zeros((batch_size, len(data[0]), self.vocab_size))
                 for i in xrange(batch_size):
                     for j in xrange(len(data[0])):
-                        one_hot_data[i][j][min(self.max_vocab, data[i][j])]=1.0
-                        one_hot_label[i][j][min(label[i][j], self.max_vocab)]=1.0
-                return one_hot_data, one_hot_label
+                        if data[i][j]>0:
+                            one_hot_data[i][j][min(self.max_vocab, data[i][j])]=1.0
+                        if label[i][j]>0:
+                            one_hot_label[i][j][min(label[i][j], self.max_vocab)]=1.0
+                return one_hot_data, one_hot_label, n_len
 
 	def json_parse(self, filename):
 		g = gzip.open(filename, 'r')
@@ -78,19 +90,20 @@ class Amazon_data(object):
 
 		f = open('temp_token.txt', 'r')
                 vocab_map = {}
-		vocab_idx = 1
+		vocab_idx = 2 # 1 represent unkown, 0 represent mask 
 		max_len = 0
 		num_S = -1
-		num_W = -1
                 data = NP.zeros((max_items+1, max_time))
 		slist = f.readlines()
 		for s in slist:
-			num_S += 1
                         #print num_S, 
-			words = re.split(' |\n', s)
+			words = re.split(' |\n|\.|!|\?|;', s)
+                        if len(words)*1.5>max_time:
+                                continue
 			if max_len < len(words):
 				max_len = len(words)
-                        num_W = 0
+                        num_S += 1
+                        num_W = -1
                         print 'Sent ', num_S
 			for w in words:
                                 if w in ['', '\n', ' ', '\t', '\r']:
@@ -107,10 +120,15 @@ class Amazon_data(object):
                                                 data[num_S][num_W]=vocab_map[w]
 					else:
 						data[num_S][num_W]=vocab_map[w]
+                                else:
+                                        data[num_S][num_W]=1
 
 		f.close()
                 max_len = min(max_time, max_len)
-                self.emb_data = data[:,:-1]
-                self.emb_label = data[:,1:]
+                self.emb_data = data[:num_S,:-1]
+                self.emb_data[:,-1]=0
+                self.emb_label = data[:num_S,1:]
+                self.emb_label[:,-1]=0
                 self.vocab_size = vocab_idx + 1
+                self.vocab_map = vocab_map
 
--- apr_word_lm.py
+++ apr_word_lm.py
@@ -11,7 +11,7 @@ import sys
 
 fname = 'apr_W'
 train_batch_size = 16
-test_batch_size = 16
+test_batch_size = 32
 test_his = []
 
 #opt here
@@ -24,39 +24,57 @@ in_dim = amazon_data.vocab_size
 out_dim = amazon_data.vocab_size
 starts = T.matrix()
 model = Model()
-def _step(cur_in, trash, prev_h1, prev_h2, prev_h3):
-    gru1 = model.gru(cur_in = cur_in, rec_in = prev_h1, name = 'gru1', shape = (in_dim, 200))
-    gru2 = model.gru(cur_in = gru1, rec_in = prev_h2, name = 'gru2', shape = (200, 200))
-    gru3 = model.gru(cur_in = gru2, rec_in = prev_h3, name = 'gru3', shape = (200, 200))
 
-    fc1 = NN.softmax(model.fc(cur_in = gru3, name = 'fc1', shape = (200, out_dim)))
+def get_mask(cur_in, mask_value=0.):
+    return T.shape_padright(T.any((1. - T.eq(cur_in, mask_value)), axis=-1))
+def masked(cur_in, mask):
+    return cur_in * mask
+def _step(cur_in, trash, prev_h1):
+    mask = get_mask(cur_in)
+    gru1 = masked(model.gru(cur_in = cur_in, rec_in = prev_h1, name = 'gru1', shape = (in_dim, 256)), mask)
+    #gru2 = masked(model.gru(cur_in = gru1, rec_in = prev_h2, name = 'gru2', shape = (200, 200)), mask)
+    #gru3 = masked(model.gru(cur_in = gru2, rec_in = prev_h3, name = 'gru3', shape = (200, 200)), mask)
 
-    return fc1, gru1, gru2, gru3
+    fc1 = masked(NN.softmax(model.fc(cur_in = gru1, name = 'fc1', shape = (256, out_dim))), mask)
+
+    return fc1, gru1
 _word_seq = word_seq.dimshuffle(1, 0, 2)
-sc, _ = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts, T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200))], truncate_gradient=200)
+#sc, _ = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts, T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200))], truncate_gradient=200)
+sc, _ = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts, T.zeros((word_seq.shape[0], 256))], truncate_gradient=200)
 word_out = sc[0].dimshuffle(1, 0, 2)
 
 EPSI = 1e-6
 cost = T.mean(NN.categorical_crossentropy(T.clip(word_out, EPSI, 1.0-EPSI), label_seq))
 test_func = theano.function([word_seq, label_seq, starts], [cost, word_out], allow_input_downcast=True)
-train_func = theano.function([word_seq, label_seq, starts], [cost, word_out], updates=rmsprop(cost, model.weightsPack.getW_list()), allow_input_downcast=True)
+train_func = theano.function([word_seq, label_seq, starts], [cost, word_out], updates=rmsprop(cost, model.weightsPack.getW_list(), lr=1e-2, epsilon=1e-4), allow_input_downcast=True)
 
 for i in xrange(50):
-    for j in xrange(1000):
-        data, label = amazon_data.get_batch(train_batch_size)
+    if i == 0:
+        data, label, n_len = amazon_data.get_batch(train_batch_size)
+        n_cost, net_out = test_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
+        NP.savez('apr_first_show.npz', label=label, predict=net_out)
+    for j in xrange(200):
+        data, label, n_len = amazon_data.get_batch(train_batch_size)
         #data = NP.zeros((train_batch_size, len(data[0]), len(data[0][0])))
         n_cost, net_out = train_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
-        acc = 1.0 * NP.mean(NP.argmax(net_out, 2)==NP.argmax(label, 2))
-        print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc
+        acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(NP.any(label>0, axis=-1))
+        per = 1.0 * NP.mean(NP.sum(-1.0*label*NP.log(net_out+EPSI), axis=(1, 2))/n_len)
+        print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc, ' Per = ', per, ' Sent Len = ', NP.mean(n_len)
+
+    NP.savez('apr_train_show.npz', label=label, predict=net_out)
 
     test_acc = []
+    test_per = []
     for j in xrange(amazon_data.test_size/test_batch_size):
-        data, label = amazon_data.get_batch(test_batch_size, test=True)
-        n_cost, net_out = test_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
-        acc = 1.0 * NP.mean(NP.argmax(net_out, 2)==NP.argmax(label, 2))
+        data, label, n_len = amazon_data.get_batch(test_batch_size, test=True)
+        n_cost, net_out = test_func(data, label, NP.zeros((test_batch_size, len(data[0][0]))))
+        acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(NP.any(label>0, axis=-1))
+        per = 1.0 * NP.mean(NP.sum(-1.0*label*NP.log(net_out+EPSI), axis=(1, 2))/n_len)
+        print ' Test Batch = ', j,  
         test_acc.append(acc)
-    print 'Epoch = ', str(i), ' Test ACC = ', NP.mean(NP.asarray(test_acc))
-    test_his.append(NP.mean(NP.asarray(test_acc)))
+        test_per.append(per)
+    print '\nEpoch = ', str(i), ' Test Acc = ', NP.mean(NP.asarray(test_acc)), ' Test Per = ', NP.mean(NP.asarray(test_per))
+    test_his.append(NP.mean(NP.asarray(test_per)))
     model.save(fname)
 model.save(fname)
 NP.savez(fname+'_result.npz', test_his = test_his)
