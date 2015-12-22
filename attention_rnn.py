b0ef571c80675392dcd91c307b33892c04a5bc71
Qipeng Guo <qpguo12@fudan.edu.cn>
2015-12-22 11:42:17 +0800

    optimize att_rnn

--- attention_rnn.py
+++ attention_rnn.py
@@ -23,9 +23,9 @@ starts = T.matrix()
 time_step = theano.shared(NP.zeros([], dtype='int'))
 #time_step = T.zeros([], dtype='int')
 
-max_time = 250
+max_time = 199
 #mem_bank time, batch, channels
-mem_bank = theano.shared(NP.zeros((1, train_batch_size, 256), dtype=theano.config.floatX))
+mem_bank = theano.shared(NP.zeros((max_time, train_batch_size, 256), dtype=theano.config.floatX))
 #mem_bank = T.zeros((1,word_seq.shape[0],200))
 model = Model()
 
@@ -66,13 +66,11 @@ def _step(cur_in, trash):
         updates[mem_bank]=new_mem_bank
         return fc2, updates
 '''
-def _step(seq_in):
-	result = T.zeros((1, seq_in.shape[1], out_dim))
-	for tick in xrange(max_time):
-		mask = get_mask(seq_in[tick])
-		fc1 = mask * T.tanh(model.fc(cur_in = seq_in[tick], name='fc1', shape=(in_dim, 200)))
-		att = model.att_mem(cur_in = fc1, mem_in = mem_bank, name = 'att1', shape= (200, 256), tick=tick)
-		
+def _step(cur_in, tick, trash, mem):
+    mask = get_mask(cur_in)
+    fc1 = mask * T.tanh(model.fc(cur_in = cur_in, name = 'fc1', shape =(in_dim, 200)))
+    att = model.att_mem(cur_in = fc1, mem_in = mem_bank, name = 'att1', shape = (200, 256), tick=tick)
+
     rec_in1 = batched_dot(mem_bank[:tick+1].dimshuffle([1, 2, 0]), att.dimshuffle([1, 0, 'x']))
     rec_in1 = T.extra_ops.squeeze(T.patternbroadcast(rec_in1, (False, False, True)))
     gru1 = mask * model.gru(cur_in = fc1, rec_in = rec_in1, name = 'gru1', shape = (200, 256))
@@ -81,25 +79,22 @@ def _step(seq_in):
 
     fc2 = mask * NN.softmax(model.fc(cur_in = gru1, name = 'fc2', shape = (256, out_dim)))
 
-    mem_bank = T.concatenate((mem_bank, gru1.dimshuffle(['x', 0, 1])), axis=0)
-    result = T.concatenate((fc2.dimshuffle(['x', 0, 1])), axis=0)
-  return result[1:]
+    mem = T.set_subtensor(mem[time_step], gru1)
+    
+    return fc2, mem
     
 _word_seq = word_seq.dimshuffle(1, 0, 2)
-'''
-#sc, _ = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts, T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200)), T.zeros((word_seq.shape[0], 200))], truncate_gradient=200)
-sc, sc_updates = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts])
-'''
-sc = _step(_word_seq)
-word_out = sc.dimshuffle(1, 0, 2)
+sc, _ = theano.scan(_step, sequences=[_word_seq, T.arange(_word_seq.shape[0])], outputs_info=[T.zeros((word_seq.shape[0], int(out_dim))), mem_bank])
+#sc, sc_updates = theano.scan(_step, sequences=[_word_seq], outputs_info=[starts])
+word_out = sc[0].dimshuffle(1, 0, 2)
 
 EPSI = 1e-6
 cost = T.sum(NN.categorical_crossentropy(T.clip(word_out, EPSI, 1.0-EPSI), label_seq))
 #test_func = theano.function([word_seq, label_seq, starts], [cost, word_out], updates=sc_updates, allow_input_downcast=True)
-test_func = theano.function([word_seq, label_seq, starts], [cost, word_out], allow_input_downcast=True)
+test_func = theano.function([word_seq, label_seq], [cost, word_out], allow_input_downcast=True)
 grad = rmsprop(cost, model.weightsPack.getW_list(), lr=1e-2, epsilon=1e-4)
 #train_func = theano.function([word_seq, label_seq, starts], [cost, word_out], updates=merge_OD(sc_updates,grad), allow_input_downcast=True)
-train_func = theano.function([word_seq, label_seq, starts], [cost, word_out], updates=grad, allow_input_downcast=True)
+train_func = theano.function([word_seq, label_seq], [cost, word_out], updates=grad, allow_input_downcast=True)
 
 
 for i in xrange(50):
@@ -107,14 +102,14 @@ for i in xrange(50):
         data, label, n_len = amazon_data.get_batch(test_batch_size)
         print NP.shape(data)
         print NP.shape(label)
-        n_cost, net_out = test_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
+        n_cost, net_out = test_func(data, label)
         print NP.shape(net_out)
         NP.savez(fname+'_first_show.npz', label=label, predict=net_out)
         #sys.exit()
     for j in xrange(200):
         data, label, n_len = amazon_data.get_batch(train_batch_size)
         #data = NP.zeros((train_batch_size, len(data[0]), len(data[0][0])))
-        n_cost, net_out = train_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
+        n_cost, net_out = train_func(data, label)
         acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(NP.any(label>0, axis=-1))
         per = 1.0 * NP.mean(NP.sum(-1.0*label*NP.log(net_out+EPSI), axis=(1, 2))/n_len)
         print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc, ' Per = ', per, ' Sent Len = ', NP.mean(n_len)
@@ -123,7 +118,7 @@ for i in xrange(50):
     test_per = []
     for j in xrange(amazon_data.test_size/test_batch_size):
         data, label, n_len = amazon_data.get_batch(test_batch_size, test=True)
-        n_cost, net_out = test_func(data, label, NP.zeros((train_batch_size, len(data[0][0]))))
+        n_cost, net_out = test_func(data, label)
         acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(NP.any(label>0, axis=-1))
         per = 1.0 * NP.mean(NP.sum(-1.0*label*NP.log(net_out+EPSI), axis=(1, 2))/n_len)
         print ' Test Batch = ', j,  
