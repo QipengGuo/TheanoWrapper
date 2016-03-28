c404365fbfc7a1543b82ea56d3a878b1adff051c
Qipeng Guo <qpguo12@fudan.edu.cn>
2016-03-28 20:40:59 +0800

    update mem alone model

--- examples/mscale_mem_alone.py
+++ examples/mscale_mem_alone.py
@@ -5,10 +5,10 @@ import theano.tensor.nnet as NN
 from wrapper import *
 from ptb_data_word import *
 fname = 'ptb_mem_alone'
-ptb = ptb_data()
+ptb = ptb_data(write_dict=True)
 in_dim = ptb.c_dim
 out_dim = ptb.c_dim
-MAX_TIME = ptb.MAX_TIME
+DICT_SIZE = 50
 gru1_dim = 200
 gru2_dim = 200
 word_out_dim = ptb.w_dim
@@ -21,14 +21,17 @@ char_target = T.tensor3()
 cw_mask = T.matrix()
 word_target = T.tensor3()
 model = Model()
-#model.load(fname)
+#model.load(fname+'_'+str(229))
 def get_mask(cur_in, mask_value=0.):
     return T.shape_padright(T.any((1. - T.eq(cur_in, mask_value)), axis=-1))
 
+# a b c I J input
+# b c I J K output
+# 0 0 1 0 0 mask
 def _step(char, mask, trash, prev_h1, word_emb, h_before, mem):
 	batch_mask = get_mask(char)
 	gru1 = batch_mask * model.gru(cur_in = char, rec_in = prev_h1, name = 'gru1', shape = (in_dim, gru1_dim))
-	att = model.att_mem(cur_in = gru1-h_before, mem_in = mem, name = 'att1', shape = (gru1_dim, mem_dim), tick=MAX_TIME)
+	att = model.att_mem(cur_in = gru1-h_before, mem_in = mem, name = 'att1', shape = (gru1_dim, mem_dim), tick=DICT_SIZE)
         mask =T.shape_padright(mask)
 	mem_in = T.sum(mem.dimshuffle([1, 0, 2]) * att.dimshuffle([1, 0, 'x']), axis=1)
 	word_emb = mem_in * mask + word_emb * (1-mask)
@@ -39,9 +42,10 @@ def _step(char, mask, trash, prev_h1, word_emb, h_before, mem):
 
 def word_step(word_emb, mask, trash, prev_h1):
 	batch_mask = get_mask(mask)
-	gru1 = batch_mask * model.gru(cur_in = word_emb, rec_in = prev_h1, name = 'gru_word', shape = (mem_dim, word_out_dim))
+        mask = T.shape_padright(mask)
+	gru1 = batch_mask * model.gru(cur_in = word_emb, rec_in = prev_h1, name = 'gru_word', shape = (mem_dim, gru2_dim))
 	next_h1 = mask * gru1 + (1-mask) * prev_h1
-	fc1 = batch_mask * NN.softmax(model.fc(cur_in = gru1, name = 'fc_word', shape = (gru2_dim, word_out_dim)))
+	fc1 = mask * batch_mask * NN.softmax(model.fc(cur_in = gru1, name = 'fc_word', shape = (gru2_dim, word_out_dim)))
 	return fc1, next_h1
 
 #NULL a b c I J    input
@@ -51,50 +55,56 @@ def word_step(word_emb, mask, trash, prev_h1):
 #NULL  K J I c b
 #K     J I c b a
 #1     0 0 1 0 0 1
-mem = model.Wmatrix(name='mem', shape=(MAX_TIME, mem_dim)).dimshuffle(0, 'x', 1)
+mem = model.Wmatrix(name='mem', shape=(DICT_SIZE, mem_dim)).dimshuffle(0, 'x', 1)
 sc, _ = theano.scan(_step, sequences=[char_in.dimshuffle([1, 0, 2]), cw_mask.dimshuffle([1, 0])], outputs_info = [T.zeros((char_in.shape[0], out_dim)),T.zeros((char_in.shape[0], gru1_dim)), T.zeros((char_in.shape[0], mem_dim)), T.zeros((char_in.shape[0], gru1_dim))], non_sequences=[mem])
 
 char_out = sc[0].dimshuffle([1, 0, 2])
 char_out = char_out[:, :-1]
-EPSI = 1e-6
+EPSI = 1e-15
 cost = T.mean(NN.categorical_crossentropy(T.clip(char_out, EPSI, 1.0-EPSI), char_target[:,1:]))
 
 enc = sc[-2]
-sc,_ = theano.scan(word_step, sequences=[enc, T.concatenate((T.ones((char_in.shape[0], 1)), cw_mask[:,:-1]), axis=1).dimshuffle([1, 0])], outputs_info = [T.zeros((char_in.shape[0], word_out_dim)), T.zeros((char_in.shape[0], gru2_dim))])
+sc,_ = theano.scan(word_step, sequences=[enc, cw_mask.dimshuffle([1, 0])], outputs_info = [T.zeros((char_in.shape[0], word_out_dim)), T.zeros((char_in.shape[0], gru2_dim))])
 
 word_out = (sc[0]).dimshuffle([1, 0, 2])
 
 cost += T.mean(NN.categorical_crossentropy(T.clip(word_out, EPSI, 1.0-EPSI), word_target))
 
 
-grad = rmsprop(cost, model.weightsPack.getW_list(), lr=1e-2, epsilon=1e-4)
-train_func = theano.function([char_in, cw_mask, char_target, word_target], [cost, char_out, word_out], updates=grad, allow_input_downcast=True)
 test_func = theano.function([char_in, cw_mask, char_target, word_target], [cost, char_out, word_out], allow_input_downcast=True)
+print 'TEST COMPILE'
+grad = rmsprop(cost, model.weightsPack.getW_list(), lr=1e-3, epsilon=1e-6)
+train_func = theano.function([char_in, cw_mask, char_target, word_target], [cost, char_out, word_out], updates=grad, allow_input_downcast=True)
+print 'TRAIN COMPILE'
 
 def evaluate(net_out, label):
-        acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(NP.any(label>0, axis=-1))
-        per = NP.sum(-1.0*label*NP.log(net_out+EPSI))/NP.sum(label>0)
+        acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(label>0)
+#        print  'DEBUG ', NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1), 'VALID', NP.sum(label>0)
+        net_out += EPSI
+        net_out /= NP.sum(net_out, axis=2)[:,:,NP.newaxis]
+        per = NP.sum(-1.0*label*NP.log(net_out))/NP.sum(label>0)
         return acc, per
        
 
 test_his = []
-for i in xrange(200):
+for i in xrange(0, 500):
     for j in xrange(500):
-        data, label, mask  = ptb.get_batch(train_batch_size)
-        n_cost, net_out, net_dec = train_func(data, mask, label)
+        data, label, mask, word_label  = ptb.get_batch(train_batch_size)
+        #print NP.shape(word_label)
+        n_cost, net_out, net_dec = train_func(data, mask, label, word_label)
         acc, per = evaluate(net_out, label[:,1:])
-        dec_acc, dec_per = evaluate(net_dec, label)
-        print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc, ' Per = ', per, 'Dec ACC = ', dec_acc, ' Per = ', dec_per
+        dec_acc, dec_per = evaluate(net_dec, word_label)
+        print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc, ' Per = ', per, 'Dec ACC = ', dec_acc, ' Per = ', dec_per, ' LEN = ', NP.shape(data)[1]
  
     test_acc = []
     test_per = []
     test_dacc = []
     test_dper = []
     for j in xrange(ptb.test_size/test_batch_size):
-        data, label, mask = ptb.get_batch(test_batch_size, test=True)
-        n_cost, net_out, net_dec = test_func(data, mask, label)
+        data, label, mask, word_label = ptb.get_batch(test_batch_size, test=True)
+        n_cost, net_out, net_dec = test_func(data, mask, label, word_label)
         acc, per = evaluate(net_out, label[:,1:])
-        dec_acc, dec_per = evaluate(net_dec, label)
+        dec_acc, dec_per = evaluate(net_dec, word_label)
         print ' Test Batch = ', j,  
         test_acc.append(acc)
         test_per.append(per)
--- examples/ptb_data_word.py
+++ examples/ptb_data_word.py
@@ -10,130 +10,189 @@ def shuffle_union(a, b):
 	p = NP.random.permutation(len(a))
 	return a[p], b[p]
 
+def shuffle(a):
+	p = NP.random.permutation(len(a))
+	return a[p]
+
 class ptb_data(object):
 
-	def __init__(self, train_fname='ptb_train', test_fname='ptb_test'):	
+	def __init__(self, train_fname='ptb_train', test_fname='ptb_valid', write_dict=False, only_dict=False):	
+                self.only_dict = only_dict
                 self.train_fname=train_fname
                 self.test_fname=test_fname
-		self.c_dict = {}
-		self.Wdict = {}
-		self.num2c = {}
-		self.w_dim = 0
-		self.c_dict_cnt = 0
-		self.MAX_TIME = 50
-		self.train_data, self.train_segs = self.read_txt(train_fname)
-		self.test_data, self.test_segs = self.read_txt(test_fname)
+                if only_dict:
+                    f = NP.load('PTB_dict.npz')
+                    self.c_dict = f['c_dict'].tolist()
+                    self.Wdict = f['Wdict'].tolist()
+                    self.num2c = f['num2c'].tolist()
+                    self.num2w = f['num2w'].tolist()
+                    self.w_dim = f['w_dim'].tolist()
+                    self.c_dict_cnt = f['c_dict_cnt'].tolist()
+                else:
+    		    self.c_dict = {}
+		    self.Wdict = {}
+		    self.num2c = {}
+                    self.num2w = {}
+		    self.w_dim = 1
+		    self.c_dict_cnt = 1
+
+                self.pcnt=0
+                self.test_data_idx = 0
+                self.train_data_idx = 0
+                self.test_len_idx = 0
+                self.train_len_idx = 0
+                self.idx_spliter = [50, 100, 150, 200, 250, 300]
+		self.MAX_TIME = 300
+		self.train_data, self.train_segs, self.train_idx = self.read_txt(train_fname)
+		self.test_data, self.test_segs, self.test_idx = self.read_txt(test_fname)
+                self.train_idx_list = self.split_len(self.train_idx)
+                self.test_idx_list = self.split_len(self.test_idx)
 		self.c_dim = self.c_dict_cnt
 		self.test_size = len(self.test_data)
+                print 'Data Size ', self.w_dim
+                if write_dict:
+                    print 'Writing Dcit'
+                    NP.savez('PTB_dict', Wdict=self.Wdict, c_dict=self.c_dict, num2c=self.num2c, num2w=self.num2w, c_dict_cnt=self.c_dict_cnt, w_dim=self.w_dim)
 
-        def get_dict(self, fname =''):
-            self.wdict = []
-            f=open(fname,'r')
-            for s in f.readlines():
-                for w in s.split(' '):
-                    if w==' ' or w=='' or w=='\n':
-                        continue
-                    if w not in self.wdict:
-                        self.wdict.append(w)
-            f.close()
+        def split_len(self, idx):
+                idx_list = []
+                for j in self.idx_spliter:
+                    idx_list.append([])
 
-        def get_word_dict(self, fname = 'ptb_dict'):
-                self.get_dict(self.train_fname)
-                self.get_dict(self.test_fname)
-                ws = []
-                for s in self.wdict:
-                    w = []
-                    for c in s:
-                        if c==' ':
-                            continue
-                        w.append(self.c_dict[c])
-                        w.extend([-1]*(self.MAX_TIME-len(w)))
-                    ws.append(w)
-                data = NP.asarray([[list_to_nparray(x, self.c_dim) for x in batch] for batch in ws])
-                return data, slist
+                for i in xrange(len(idx)):
+                    for j in xrange(len(self.idx_spliter)):
+                        if idx[i]<self.idx_spliter[j]:
+                            idx_list[j].append(i)
+                            break
 
+                return idx_list
 
-                
 	def get_batch(self, batch_size=1, test=False):
 		data = self.test_data if test else self.train_data
 		segs = self.test_segs if test else self.train_segs
-		data = NP.asarray(data)
-		segs = NP.asarray(segs)
-		data, segs = shuffle_union(data, segs)
-		data = data[:batch_size]
-		segs = segs[:batch_size]
-		words = NP.zeros_like(data)
-		data = NP.asarray([[list_to_nparray(x, self.c_dim) for x in batch] for batch in data])
-		label = data
-		mask = NP.zeros((batch_size, self.MAX_TIME))
+                idx_list = self.test_idx_list if test else self.train_idx_list
+                data_idx = self.test_data_idx if test else self.train_data_idx
+                len_idx = self.test_len_idx if test else self.train_len_idx
+                data_idx += batch_size
+                if data_idx>=len(data):
+                    for i in xrange(len(idx_list)):
+                        idx_list[i]=shuffle(idx_list[i])
+                    data_idx = batch_size
+                    len_idx = 0
+                if data_idx>=len(idx_list[len_idx]):
+                    len_idx+=1
+                    while len_idx<len(self.idx_spliter) and len(idx_list[len_idx])==0:
+                        len_idx+=1
+                    if len_idx>=len(self.idx_spliter):
+                        len_idx = 0
+                    data_idx = batch_size
+                if test:
+                    self.test_len_idx = len_idx
+                    self.test_data_idx = data_idx
+                else:
+                    self.train_len_idx = len_idx
+                    self.train_data_idx = data_idx
+                idx = NP.asarray(idx_list[len_idx][data_idx-batch_size:data_idx])
+                MAX_TIME = self.idx_spliter[len_idx]
+		data = data[idx]
+                tdata = []
+                for i in xrange(len(data)):
+                    tdata.append((NP.concatenate((data[i], NP.zeros(MAX_TIME-len(data[i]))-1), axis=0)).tolist())
+                data = NP.asarray(tdata)
+		segs = segs[idx]
+		words = NP.zeros_like(data)-1
+                batch_size = len(data)
+		mask = NP.zeros((batch_size, MAX_TIME))
 		for b in xrange(batch_size):
-			pos = 0 
+			pos = 0
+                        #print self.tostring(data[b])
 			for i in xrange(len(segs[b])):
 				t = segs[b][i]
 				pos += t
-				if pos>=self.MAX_TIME:
+				if pos>=MAX_TIME:
 					break
-				mask[b][pos]=1
 				if (i<len(segs[b])-1):
-					str1 = self.tostring(data[b][pos:pos+segs[b][i+1]])
+				        mask[b][pos]=1
+				        str1 = self.tostring(data[b][pos:pos+segs[b][i+1]])
+                                        str2 = self.tostring(data[b][pos-segs[b][i]:pos])
+                                        #print str2, '|||', str1
 					words[b][pos]= self.Wdict[str1]
-#			if pos<self.MAX_TIME-1:
-#				mask[b][pos+1]=-1
-                        if pos>=self.MAX_TIME:
-                                mask[b][-1] = 1
+#                        if pos>=MAX_TIME:
+#                                mask[b][-1] = 1
+                data = NP.asarray([[list_to_nparray(x, self.c_dim) for x in batch] for batch in data])
+		label = data
 		words = NP.asarray([[list_to_nparray(x, self.w_dim) for x in batch] for batch in words])
 
 		return data, label, mask, words
 	def tostring(self, char_seq):
 		str1 = ''
 		for i in char_seq:
-			str1 += self.num2c[i]
+                        #print self.num2c[i]
+                        if i not in self.num2c.keys():
+                            print 'ERROR ', i
+                        else:
+			    str1 += self.num2c[i]
+                if str1 == '':
+                    return ' '
 		return str1
 		
 	def read_txt(self, fname):
 		f=open(fname, 'r')
 		data = []
 		segs = []
+                idx = []
 		slist = f.readlines()
 		for s in slist:
 			t = s.split('|||')
 			if len(t)!=2:
 				continue
+                        #print self.pcnt
+                        self.pcnt+=1
 			one_row = []
 			one_seg = []
 			x, z = t
 			tt = 0
-			for c in x.split(' '):
-				if c == '' or c == ' ':
-					continue 
+                        xx = x.split(' ')
+                        xx = filter(lambda x:x!='' and x!=' ', xx)
+                        for c in xx:
 				tt+=1
 				if tt>=self.MAX_TIME:
 					break
 				if c not in self.c_dict.keys():
-					self.c_dict[c]=self.c_dict_cnt
-					self.c_dict_cnt += 1
-					self.num2c[self.c_dict_cnt-1]=c
+                                        if self.only_dict:
+                                            self.c_dict[c]=0
+                                        else:
+					    self.c_dict[c]=self.c_dict_cnt
+					    self.c_dict_cnt += 1
+					    self.num2c[self.c_dict_cnt-1]=c
 				one_row.append(self.c_dict[c])
 
 			tt = 0
 			for seg in z.split(' '):
 				if not seg.isdigit():
 					continue
-				tt+=1
+				tt+=int(seg)
 				if tt>=self.MAX_TIME:
 					break
 				one_seg.append(int(seg))
 
 			last_pos = 0
-			for ss in seg:
-				str1 = self.tostring(one_row[last_pos:last_pos+ss+1])
-				if str1 not in self.Wdict.keys():
-					self.Wdict[str1]=self.w_dim
-					self.w_dim += 1
-					last_pos = last_pos + ss
+                        str1 = self.tostring(one_row)
+                        #print str1
+			for ss in one_seg:
+				str1 = self.tostring(one_row[last_pos:last_pos+ss])
+				if (str1 not in self.Wdict.keys()):
+                                        #print self.w_dim, str1
+                                        if self.only_dict:
+                                            self.Wdict[str1]=0
+                                        else:
+					    self.Wdict[str1]=self.w_dim
+                                            self.num2w[self.w_dim] = str1
+					    self.w_dim += 1
+				last_pos = last_pos + ss
 
-			one_row.extend([-1]*(self.MAX_TIME-len(one_row)))
 			data.append(one_row)
 			segs.append(one_seg)
+                        idx.append(len(one_row))
 		f.close()
-		return data, segs
+		return NP.asarray(data), NP.asarray(segs), NP.asarray(idx)
