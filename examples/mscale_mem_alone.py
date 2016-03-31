eeda91653bf26d52e8a3a9217668d311be3ddfd3
Qipeng Guo <qpguo12@fudan.edu.cn>
2016-03-31 16:46:18 +0800

    add comments for mem_alone

--- examples/mscale_mem_alone.py
+++ examples/mscale_mem_alone.py
@@ -1,3 +1,4 @@
+#the code may be quite ugly, but this version is using in now running experiments, the optimization will add in next version(such as speed up cross_entropy and remove abs in attention part)
 import numpy as NP
 import theano
 import theano.tensor as T
@@ -6,19 +7,19 @@ from wrapper import *
 from ptb_data_word import *
 fname = 'ptb_mem_alone'
 ptb = ptb_data(write_dict=True)
-in_dim = ptb.c_dim
-out_dim = ptb.c_dim
-DICT_SIZE = 50
-gru1_dim = 200
-gru2_dim = 200
-word_out_dim = ptb.w_dim
-mem_dim = 100
+in_dim = ptb.c_dim # character dict size
+out_dim = ptb.c_dim 
+DICT_SIZE = 50 #our word emb dict is an 50 * 100 matrix
+gru1_dim = 200 # gru in character LM 
+gru2_dim = 200 # gru in word LM
+word_out_dim = ptb.w_dim # word dict size
+mem_dim = 100 # word emb dim
 train_batch_size = 32
 test_batch_size = 32
-mask_value = 1.0
-char_in = T.tensor3()
+mask_value = 1.0 # useless
+char_in = T.tensor3() 
 char_target = T.tensor3()
-cw_mask = T.matrix()
+cw_mask = T.matrix() #character to word mask
 word_target = T.tensor3()
 model = Model()
 #model.load(fname+'_'+str(229))
@@ -27,15 +28,18 @@ model = Model()
 #a    b c I J K    output
 #0    0 0 1 0 0 1  mask
 
+#get mask for handle variable length in batch, all zeros vector will be masked out
 def get_mask(cur_in, mask_value=0.):
     return T.shape_padright(T.any((1. - T.eq(cur_in, mask_value)), axis=-1))
 
+#character step
 def _step(char, mask, trash, prev_h1, word_emb, h_before, mem):
 	batch_mask = get_mask(char)
 	gru1 = batch_mask * model.gru(cur_in = char, rec_in = prev_h1, name = 'gru1', shape = (in_dim, gru1_dim))
-	att = model.att_mem(cur_in = gru1-h_before, mem_in = mem, name = 'att1', shape = (gru1_dim, mem_dim), tick=DICT_SIZE)
-        mask =T.shape_padright(mask)
-	mem_in = T.sum(mem.dimshuffle([1, 0, 2]) * att.dimshuffle([1, 0, 'x']), axis=1)
+	att = model.att_mem(cur_in = gru1-h_before, mem_in = mem, name = 'att1', shape = (gru1_dim, mem_dim), tick=DICT_SIZE) # the tick is useless in this task, i using it for variable length memory ...
+        mask =T.shape_padright(mask) # using our word mask
+        # mem means our word emb dict 
+	mem_in = T.sum(mem.dimshuffle([1, 0, 2]) * att.dimshuffle([1, 0, 'x']), axis=1) # using the attention to extract word emb from dict
 	word_emb = mem_in * mask + word_emb * (1-mask)
         h_before = gru1 * mask + h_before * (1-mask)
         fc1 = batch_mask * NN.softmax(model.fc(cur_in = gru1, name = 'fc1', shape = (gru1_dim, out_dim)))
@@ -46,23 +50,31 @@ def word_step(word_emb, mask, trash, prev_h1):
 	batch_mask = get_mask(mask)
         mask = T.shape_padright(mask)
 	gru1 = batch_mask * model.gru(cur_in = word_emb, rec_in = prev_h1, name = 'gru_word', shape = (mem_dim, gru2_dim))
+        # skip the time step which mask equal zero, this is the reason why we training so slowly
 	next_h1 = mask * gru1 + (1-mask) * prev_h1
 	fc1 = mask * batch_mask * NN.softmax(model.fc(cur_in = gru1, name = 'fc_word', shape = (gru2_dim, word_out_dim)))
 	return fc1, next_h1
 
+#define the word emb dict
 mem = model.Wmatrix(name='mem', shape=(DICT_SIZE, mem_dim)).dimshuffle(0, 'x', 1)
+#scan for character step
 sc, _ = theano.scan(_step, sequences=[char_in.dimshuffle([1, 0, 2]), cw_mask.dimshuffle([1, 0])], outputs_info = [T.zeros((char_in.shape[0], out_dim)),T.zeros((char_in.shape[0], gru1_dim)), T.zeros((char_in.shape[0], mem_dim)), T.zeros((char_in.shape[0], gru1_dim))], non_sequences=[mem])
 
 char_out = sc[0].dimshuffle([1, 0, 2])
 char_out = char_out[:, :-1]
 EPSI = 1e-15
+#loss for character LM, char_target is same with char_in, so it will be shuffle one step for LM task
 cost = T.mean(NN.categorical_crossentropy(T.clip(char_out, EPSI, 1.0-EPSI), char_target[:,1:]))
 
+#encoder information, word emb
 enc = sc[-2]
+
+#scan for word LM, char_in.shape[0] is the batch_size ...
 sc,_ = theano.scan(word_step, sequences=[enc, cw_mask.dimshuffle([1, 0])], outputs_info = [T.zeros((char_in.shape[0], word_out_dim)), T.zeros((char_in.shape[0], gru2_dim))])
 
 word_out = (sc[0]).dimshuffle([1, 0, 2])
 
+#loss for word LM task, word_target is tensor now, it will be matrix in next version
 cost += T.mean(NN.categorical_crossentropy(T.clip(word_out, EPSI, 1.0-EPSI), word_target))
 
 
@@ -72,6 +84,7 @@ grad = rmsprop(cost, model.weightsPack.getW_list(), lr=1e-3, epsilon=1e-6)
 train_func = theano.function([char_in, cw_mask, char_target, word_target], [cost, char_out, word_out], updates=grad, allow_input_downcast=True)
 print 'TRAIN COMPILE'
 
+#calculate the Acc and PPL
 def evaluate(net_out, label):
         acc = 1.0 * NP.sum((NP.argmax(net_out, 2)==NP.argmax(label, 2)).astype('int')+(NP.any(label>0, axis=-1)).astype('int')>1)/NP.sum(label>0)
         net_out += EPSI
@@ -86,10 +99,11 @@ for i in xrange(0, 500):
         data, label, mask, word_label  = ptb.get_batch(train_batch_size)
         #print NP.shape(word_label)
         n_cost, net_out, net_dec = train_func(data, mask, label, word_label)
-        acc, per = evaluate(net_out, label[:,1:])
-        dec_acc, dec_per = evaluate(net_dec, word_label)
+        acc, per = evaluate(net_out, label[:,1:]) # i should using BPC here later
+        dec_acc, dec_per = evaluate(net_dec, word_label) # wrong name from previous code, not decode, it is word Acc and word PPL
         print 'Epoch = ', str(i), ' Batch = ', str(j), ' Cost = ', n_cost, ' ACC = ', acc, ' Per = ', per, 'Dec ACC = ', dec_acc, ' Per = ', dec_per, ' LEN = ', NP.shape(data)[1]
  
+    # test part, same with training
     test_acc = []
     test_per = []
     test_dacc = []
--- examples/ptb_data_word.py
+++ examples/ptb_data_word.py
@@ -1,3 +1,4 @@
+#the code may be quite ugly, but this version is using in now running experiments, the optimization will add in next version(such as speed up cross_entropy and remove abs in attention part)
 import numpy as NP
 
 def list_to_nparray(x, dim):
@@ -16,10 +17,18 @@ def shuffle(a):
 
 class ptb_data(object):
 
+# an example for data in ptb_train, easy for generate segmentation mask
+# n o i t w a s n ' t b l a c k m o n d a y ||| 2 2 3 3 5 6 
+
 	def __init__(self, train_fname='ptb_train', test_fname='ptb_valid', write_dict=False, only_dict=False):	
-                self.only_dict = only_dict
+                self.only_dict = only_dict # only_dict means using exist dict, don't add new word when parsing file
                 self.train_fname=train_fname
                 self.test_fname=test_fname
+                # c_dict, dictionary for character
+                # Wdict, dictionary for word
+                # num2c, index in dict to character
+                # num2w, index in dict to word
+                # w_dim, c_dim, c_dict_cnt are count of word and character, last one is redundant
                 if only_dict:
                     f = NP.load('PTB_dict.npz')
                     self.c_dict = f['c_dict'].tolist()
@@ -35,18 +44,22 @@ class ptb_data(object):
                     self.num2w = {}
 		    self.w_dim = 1
 		    self.c_dict_cnt = 1
-
-                self.pcnt=0
+                #w_dim, c_dict_cnt start from 1, actually it is useless, just keep the 0 to special case
+                self.pcnt=0 #debug count, useless
+                # follow index record the position which next batch start
                 self.test_data_idx = 0
                 self.train_data_idx = 0
+                # the len_idx record which length group should be used
                 self.test_len_idx = 0
                 self.train_len_idx = 0
-                self.idx_spliter = [50, 100, 150, 200, 250, 300]
-		self.MAX_TIME = 300
+                self.idx_spliter = [50, 100, 150, 200, 250, 300] #split the sentence length in group to speed up, the 50 contains all sentences which length smaller than 50, the batch length will be 50
+		self.MAX_TIME = 300 #one sentences length no more than 300 characters 
+                #read from file and split into group
 		self.train_data, self.train_segs, self.train_idx = self.read_txt(train_fname)
 		self.test_data, self.test_segs, self.test_idx = self.read_txt(test_fname)
                 self.train_idx_list = self.split_len(self.train_idx)
                 self.test_idx_list = self.split_len(self.test_idx)
+
 		self.c_dim = self.c_dict_cnt
 		self.test_size = len(self.test_data)
                 print 'Data Size ', self.w_dim
@@ -54,6 +67,7 @@ class ptb_data(object):
                     print 'Writing Dcit'
                     NP.savez('PTB_dict', Wdict=self.Wdict, c_dict=self.c_dict, num2c=self.num2c, num2w=self.num2w, c_dict_cnt=self.c_dict_cnt, w_dim=self.w_dim)
 
+        #split the sample by length, only record index
         def split_len(self, idx):
                 idx_list = []
                 for j in self.idx_spliter:
@@ -67,6 +81,7 @@ class ptb_data(object):
 
                 return idx_list
 
+        # the data structure may be stupid, ... anyway, i add these function step by step, i will reconstruct the code after checking the correctness
 	def get_batch(self, batch_size=1, test=False):
 		data = self.test_data if test else self.train_data
 		segs = self.test_segs if test else self.train_segs
@@ -74,11 +89,13 @@ class ptb_data(object):
                 data_idx = self.test_data_idx if test else self.train_data_idx
                 len_idx = self.test_len_idx if test else self.train_len_idx
                 data_idx += batch_size
+                #shuffle the data when pass through the whole data once
                 if data_idx>=len(data):
                     for i in xrange(len(idx_list)):
                         idx_list[i]=shuffle(idx_list[i])
                     data_idx = batch_size
                     len_idx = 0
+                #enter next length group
                 if data_idx>=len(idx_list[len_idx]):
                     len_idx+=1
                     while len_idx<len(self.idx_spliter) and len(idx_list[len_idx])==0:
@@ -86,44 +103,55 @@ class ptb_data(object):
                     if len_idx>=len(self.idx_spliter):
                         len_idx = 0
                     data_idx = batch_size
+
                 if test:
                     self.test_len_idx = len_idx
                     self.test_data_idx = data_idx
                 else:
                     self.train_len_idx = len_idx
                     self.train_data_idx = data_idx
-                idx = NP.asarray(idx_list[len_idx][data_idx-batch_size:data_idx])
+                # get index from group
+                idx = NP.asarray(idx_list[len_idx][data_idx-batch_size:data_idx]) 
                 MAX_TIME = self.idx_spliter[len_idx]
+                # extract data by index, all the extracted sentences' length smaller than local MAX_TIME now, so the batch length is MAX_TIME
 		data = data[idx]
                 tdata = []
+                # data is still the index , not one-hot, i using -1 to represent all zeros, the following things compelete the sentences to batch length
                 for i in xrange(len(data)):
                     tdata.append((NP.concatenate((data[i], NP.zeros(MAX_TIME-len(data[i]))-1), axis=0)).tolist())
+
                 data = NP.asarray(tdata)
 		segs = segs[idx]
+                # -1 means all zeros in one-hot
 		words = NP.zeros_like(data)-1
+                # fitthe data when the length is not enough for batch_size
                 batch_size = len(data)
 		mask = NP.zeros((batch_size, MAX_TIME))
 		for b in xrange(batch_size):
-			pos = 0
+			pos = 0 # positions in character seqences
                         #print self.tostring(data[b])
 			for i in xrange(len(segs[b])):
-				t = segs[b][i]
+				t = segs[b][i] #the length of this word 
 				pos += t
-				if pos>=MAX_TIME:
+				if pos>=MAX_TIME: #useless checking ...
 					break
-				if (i<len(segs[b])-1):
-				        mask[b][pos]=1
-				        str1 = self.tostring(data[b][pos:pos+segs[b][i+1]])
+				if (i<len(segs[b])-1): #stop before last word, because there no next word for last word
+				        mask[b][pos]=1 #mask equal one means the end of one word
+                                        #str1 is the next word, str2 is the current word
+				        str1 = self.tostring(data[b][pos:pos+segs[b][i+1]]) 
                                         str2 = self.tostring(data[b][pos-segs[b][i]:pos])
                                         #print str2, '|||', str1
-					words[b][pos]= self.Wdict[str1]
+					words[b][pos]= self.Wdict[str1] # string to index
 #                        if pos>=MAX_TIME:
 #                                mask[b][-1] = 1
+                # to one hot representation
                 data = NP.asarray([[list_to_nparray(x, self.c_dim) for x in batch] for batch in data])
 		label = data
 		words = NP.asarray([[list_to_nparray(x, self.w_dim) for x in batch] for batch in words])
 
 		return data, label, mask, words
+
+        # convert character sequences to word string
 	def tostring(self, char_seq):
 		str1 = ''
 		for i in char_seq:
@@ -132,7 +160,7 @@ class ptb_data(object):
                             print 'ERROR ', i
                         else:
 			    str1 += self.num2c[i]
-                if str1 == '':
+                if str1 == '': # only for debug, space and null are not in our dict
                     return ' '
 		return str1
 		
@@ -143,7 +171,7 @@ class ptb_data(object):
                 idx = []
 		slist = f.readlines()
 		for s in slist:
-			t = s.split('|||')
+			t = s.split('|||') # see the data format at the top
 			if len(t)!=2:
 				continue
                         #print self.pcnt
@@ -154,7 +182,7 @@ class ptb_data(object):
 			tt = 0
                         xx = x.split(' ')
                         xx = filter(lambda x:x!='' and x!=' ', xx)
-                        for c in xx:
+                        for c in xx: # add character 
 				tt+=1
 				if tt>=self.MAX_TIME:
 					break
@@ -164,11 +192,11 @@ class ptb_data(object):
                                         else:
 					    self.c_dict[c]=self.c_dict_cnt
 					    self.c_dict_cnt += 1
-					    self.num2c[self.c_dict_cnt-1]=c
+					    self.num2c[self.c_dict_cnt-1]=c # so ugly...
 				one_row.append(self.c_dict[c])
 
 			tt = 0
-			for seg in z.split(' '):
+			for seg in z.split(' '): # add segmentation
 				if not seg.isdigit():
 					continue
 				tt+=int(seg)
@@ -177,10 +205,10 @@ class ptb_data(object):
 				one_seg.append(int(seg))
 
 			last_pos = 0
-                        str1 = self.tostring(one_row)
+                        str1 = self.tostring(one_row) # debug
                         #print str1
 			for ss in one_seg:
-				str1 = self.tostring(one_row[last_pos:last_pos+ss])
+				str1 = self.tostring(one_row[last_pos:last_pos+ss]) # add words 
 				if (str1 not in self.Wdict.keys()):
                                         #print self.w_dim, str1
                                         if self.only_dict:
